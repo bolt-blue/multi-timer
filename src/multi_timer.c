9c135d065cbe7527cbe7e36aedb30a9a5cd0732d
bolt-blue <0x@dead.beef>
2022-03-14 21:54:39 +0400

    Implement timer display
    We want to be able to actually see the timer counting down.
    `sleep()` was insufficient for this, thus we moved to using
    `clock_gettime()` and `nanosleep()`.
    This should give us much more flexibility.
    
    Currently, we simply poll the time 10 times per second, which seems
    sufficient for the time being.
    
    Display is done directly to stdout. We need to refactor soon to
    facilitate displaying on multiple platforms, both for CLI and GUI.

--- src/multi_timer.c
+++ src/multi_timer.c
@@ -1,13 +1,52 @@
 #include "multi_timer.h"
 
-#include <unistd.h>     // sleep
+#include <time.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#define ONE_TENTH_SEC 1000000000L / 10
+
+void display_time(long s);
 
 void run_timer(mtimer_t t)
 {
     // TODO:
-    // - Display countdown
+    // - Separate out display functions (e.g. `display_time`)
+    //   - These should be handled by CLI/GUI layer, per platform
     // - Fork this process?
     //   Do we care if this blocks the application?
-    sleep(t.duration);
+
+    struct timespec wait = (struct timespec){.tv_nsec=ONE_TENTH_SEC};
+    struct timespec begin;
+    struct timespec now;
+
+    clock_gettime(CLOCK_MONOTONIC, &begin);
+
+    time_t delta, counter = 0;
+    time_t remaining = t.duration;
+
+    display_time(remaining);
+    while (remaining) {
+        nanosleep(&wait, NULL);
+        clock_gettime(CLOCK_MONOTONIC, &now);
+
+        delta = now.tv_sec - begin.tv_sec;
+        if (delta > counter && now.tv_nsec >= begin.tv_nsec) {
+            counter++;
+            remaining--;
+            display_time(remaining);
+        }
+    }
+    putc('\n', stdout);
+
     if (t.on_complete) t.on_complete(t.data);
+}
+
+void display_time(long seconds)
+{
+    int32_t h = seconds / 60 / 60;
+    int32_t m = (seconds - h * 60 * 60) / 60;
+    int32_t s = seconds - h * 60 * 60 - m * 60;
+    printf("\r%02d:%02d:%02d", h, m, s);
+    fflush(stdout);
 }
