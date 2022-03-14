40b827526c4111cc3b4cb7dacb8a98824bdde577
bolt-blue <0x@dead.beef>
2022-03-14 21:49:02 +0400

    Accept timer duration as command line arg
    Also lay the ground work for how we will want to be able to set multiple
    timers easily.

--- src/mtm.c
+++ src/mtm.c
@@ -1,13 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "multi_timer.h"
 
 // Forward declarations
 void notify(void *);
+void usage(char *pname);
 
 int main(int argc, char **argv)
 {
-    mtimer_t first_timer = new_timer(.duration=3, .on_complete=notify, .data="Ding! We're done.");
+    if (argc == 1) {
+        usage(argv[0]);
+        exit(1);
+    }
+
+#if 1
+    // TODO: Validation
+    long duration = atoi(argv[1]);
+
+    // TODO:
+    // - Allow multiple timers to be set
+    // - Allow durations to be set in seconds, minutes or hours
+    //   e.g. "10s", "3m", "1h"
+#else
+    for (int i = 1; i < argc; i++) {
+
+    }
+#endif
+
+    mtimer_t first_timer = new_timer(.duration=duration, .on_complete=notify, .data="Ding! We're done.");
 
     run_timer(first_timer);
 
@@ -23,4 +44,9 @@ void notify(void *message)
     if (!message) return;
     char *msg = (char *)message;
     printf("%s\n", msg);
+}
+
+void usage(char *pname)
+{
+    printf("Usage: %s duration [...]\n", pname);
 }
